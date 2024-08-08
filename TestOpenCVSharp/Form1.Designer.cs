6b67f873b143af9e7872edd26780278c4a73c7a1
jglatts <john@z-axiscc.com>
2024-08-08 12:15:22 -0400

    updates

--- TestOpenCVSharp/Form1.Designer.cs
+++ TestOpenCVSharp/Form1.Designer.cs
@@ -32,14 +32,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.btnFindGap = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mainFeedPicBox)).BeginInit();
             this.SuspendLayout();
             // 
             // mainFeedPicBox
             // 
-            this.mainFeedPicBox.Location = new System.Drawing.Point(121, 64);
+            this.mainFeedPicBox.Location = new System.Drawing.Point(85, 38);
+            this.mainFeedPicBox.Margin = new System.Windows.Forms.Padding(2);
             this.mainFeedPicBox.Name = "mainFeedPicBox";
-            this.mainFeedPicBox.Size = new System.Drawing.Size(599, 311);
+            this.mainFeedPicBox.Size = new System.Drawing.Size(447, 251);
             this.mainFeedPicBox.TabIndex = 0;
             this.mainFeedPicBox.TabStop = false;
             // 
@@ -47,18 +49,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(339, 13);
+            this.label1.Location = new System.Drawing.Point(256, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 48);
+            this.label1.Size = new System.Drawing.Size(117, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Live Feed";
             // 
             // btnStart
             // 
             this.btnStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnStart.Location = new System.Drawing.Point(175, 397);
+            this.btnStart.Location = new System.Drawing.Point(85, 305);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(220, 68);
+            this.btnStart.Size = new System.Drawing.Size(137, 41);
             this.btnStart.TabIndex = 2;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -67,23 +71,38 @@
             // btnStop
             // 
             this.btnStop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnStop.Location = new System.Drawing.Point(456, 397);
+            this.btnStop.Location = new System.Drawing.Point(393, 305);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(220, 68);
+            this.btnStop.Size = new System.Drawing.Size(139, 41);
             this.btnStop.TabIndex = 3;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
             // 
+            // btnFindGap
+            // 
+            this.btnFindGap.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFindGap.Location = new System.Drawing.Point(236, 305);
+            this.btnFindGap.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFindGap.Name = "btnFindGap";
+            this.btnFindGap.Size = new System.Drawing.Size(137, 41);
+            this.btnFindGap.TabIndex = 4;
+            this.btnFindGap.Text = "Find Gap";
+            this.btnFindGap.UseVisualStyleBackColor = true;
+            this.btnFindGap.Click += new System.EventHandler(this.btnFindGap_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(858, 499);
+            this.ClientSize = new System.Drawing.Size(642, 369);
+            this.Controls.Add(this.btnFindGap);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.mainFeedPicBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.mainFeedPicBox)).EndInit();
@@ -98,5 +117,6 @@
         private Label label1;
         private Button btnStart;
         private Button btnStop;
+        private Button btnFindGap;
     }
 }
--- TestOpenCVSharp/Form1.cs
+++ TestOpenCVSharp/Form1.cs
@@ -5,6 +5,7 @@ using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using DirectShowLib;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace TestOpenCVSharp
 {
@@ -29,7 +30,7 @@ namespace TestOpenCVSharp
         {
             String s = "\n";
             DsDevice[] devices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
-            
+
             if (devices.Length == 0)
             {
                 MessageBox.Show("no video devices!");
@@ -45,8 +46,8 @@ namespace TestOpenCVSharp
         private void tryOpenCam()
         {
             frame = new Mat();
-            openCam();    
-            
+            openCam();
+
             if (capture.IsOpened())
             {
                 capture.Read(frame);
@@ -67,7 +68,7 @@ namespace TestOpenCVSharp
             task.Start();
         }
 
-        private void openCam() 
+        private void openCam()
         {
             try
             {
@@ -96,25 +97,95 @@ namespace TestOpenCVSharp
             {
                 if (token.IsCancellationRequested)
                 {
-                    MessageBox.Show("stopping!");
                     break;
                 }
                 capture.Read(frame);
-                image = BitmapConverter.ToBitmap(frame);
-                if (mainFeedPicBox.Image != null)
-                {
-                    mainFeedPicBox.Image.Dispose();
-                }
-                mainFeedPicBox.Image = image;
-                Task.Delay(10);   
+                updateLiveFeedImage(frame);
+                Task.Delay(10);
             }
+        }
 
+        private void detectGap()
+        {
+            Mat src_gray = new Mat();
+            Mat src_canny = new Mat();
+            Mat src_roi = new Mat();
+            Mat src_thresh = new Mat();
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hierarchyIndexes;
+
+            capture.Read(frame);
+            src_roi = frame.SubMat(new OpenCvSharp.Range(50, frame.Rows),
+                                   new OpenCvSharp.Range(0, frame.Cols));
+            updateLiveFeedImage(src_roi);
+            Task.Delay(2000);
+
+            Cv2.CvtColor(src_roi, src_gray, ColorConversionCodes.BGR2GRAY);
+            updateLiveFeedImage(src_gray);
+            Task.Delay(2000);
+
+            Cv2.Threshold(src_gray, src_thresh, 130, 450, ThresholdTypes.TozeroInv);
+            updateLiveFeedImage(src_thresh);
+            Task.Delay(2000);
+
+            Cv2.Canny(src_thresh, src_canny, 50, 300, 3, true);
+            updateLiveFeedImage(src_canny);
+            Task.Delay(2000);
+
+            Cv2.FindContours(src_canny, out contours, out hierarchyIndexes,
+                             mode: RetrievalModes.External,
+                             method: ContourApproximationModes.ApproxNone);
+
+            MessageBox.Show("Contours Length: " + contours.Length);
+
+            //anaylzeFrame();
+        }
+
+        private void anaylzeFrame(Mat src_roi, Mat src_canny)
+        {
+            // splits the image into 2 seperate ROI's
+            // find contours of both ROI's and stores in 2 sets
+            // run convex hull on both sets of contours
+            // find distance between the convex hulls
+            // draw both sets on OG img to examine
+            // thats gap!
+        }
+
+        private void updateLiveFeedImage(Mat frame) 
+        {
+            image = BitmapConverter.ToBitmap(frame);
+            if (mainFeedPicBox.Image != null)
+            {
+                mainFeedPicBox.Image.Dispose();
+            }
+            mainFeedPicBox.Image = image;
         }
 
         private void btnStop_Click_1(object sender, EventArgs e)
         {
-            cancelTokenSource.Cancel();
-            cancelTokenSource.Dispose();
+            try
+            {
+                cancelTokenSource.Cancel();
+                cancelTokenSource.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btnFindGap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cancelTokenSource.Cancel();
+                cancelTokenSource.Dispose();
+            }
+            catch (Exception ex)
+            {
+            }
+            Task.Delay(10);
+            detectGap();
         }
     }
 }
